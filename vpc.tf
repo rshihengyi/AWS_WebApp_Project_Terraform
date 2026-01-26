a9644af2b2f800fd59363023242a3f31adf93231
rshihengyi <robertheng3@gmail.com>
2026-01-25 23:54:52 -0500

    Update vpc.tf
    fixed route table association for both subnets

--- vpc.tf
+++ vpc.tf
@@ -46,16 +46,13 @@ resource "aws_route_table" "public_rt" {
  }
 }
 
-locals {
-  subnet_ids = [
-    aws_subnet.public_1.id,
-    aws_subnet.public_2.id
-  ]
+# Route Table Associations
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-# Route Table Associations
-resource "aws_route_table_association" "public_assoc" {
-  for_each = toset(local.subnet_ids)
-  subnet_id = each.value.id
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_rt.id
 }
