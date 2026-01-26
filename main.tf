13f3a4121465c3ec7fa0b5b2f3835975380c68ec
rshihengyi <robertheng3@gmail.com>
2026-01-26 12:48:08 -0500

    updated files, successfully created vpc

--- main.tf
+++ main.tf
@@ -2,32 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#data "aws_ami" "amazon_linux_2" {
-#  most_recent = true
-#
-# filter {
-#  name   = "name"
-#   values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+# data "aws_ami" "amazon_linux_2" {
+#   most_recent = true
+
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
+#   owners = ["137112412989"] # Amazon
 # }
-# owners = ["137112412989"] # Amazon
-#}
 
-#resource "aws_alb" "lb_web" {
-#  name            = "web-alb"
-#  internal        = false
-#  load_balancer_type = "application"
-#  security_groups = [aws_security_group.sg_alb.id]
-#  subnets         = aws_subnet.public.*.id
+# resource "aws_alb" "lb_web" {
+#   name            = "web-alb"
+#   internal        = false
+#   load_balancer_type = "application"
+#   security_groups = [aws_security_group.sg_alb.id]
+#   subnets         = aws_subnet.public.*.id
 
-#  tags = {
-#    Name = "WebALB"
-#  }
-#}
+#   tags = {
+#     Name = "WebALB"
+#   }
+  
+# }
 
-#resource "ec2_instance" "vm_web" {
-#  ami           = data.aws_ami.amazon_linux_2.id
-#  instance_type = "t2.micro"
-#  tags = {
-#    Name = "WebServerInstance"
-#  }
-#}
+# resource "ec2_instance" "vm_web" {
+#   ami           = data.aws_ami.amazon_linux_2.id
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "WebServerInstance"
+#   } 
+# }
