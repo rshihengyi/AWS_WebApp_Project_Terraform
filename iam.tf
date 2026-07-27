ca89b79db3ca8cc0dcab59bc62a062b18b77a734
rshihengyi <robertheng3@gmail.com>
2026-07-27 13:41:55 -0400

    Revert "added helm configurations"
    This reverts commit e37c1af5d9e53bf939d74c0aafdef7d679264545.

--- iam.tf
+++ iam.tf
@@ -1,64 +1,160 @@
-# // IAM role for EKS worker nodes
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
 
-# /* Minimum Parameters: 
-#     - name:                 role name
-#     - assume_role_policy:   trust policy (who can wear the hat)
-# */
-# resource "aws_iam_role" "worker_nodes" {
-#   name = "Node-Join-Cluster"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+# IAM Role for SSM Agent
+resource "aws_iam_role" "ec2_ssm" {
+  name = "SSM-Command"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
-# /* Minimum Parameters: 
-#     - role:                 what role to attach permissions to
-#     - policy_arn:           the iam policy arn
-# */
-# resource "aws_iam_role_policy_attachment" "node_pa" {
-#   role       = aws_iam_role.worker_nodes.name
-#   policy_arn = aws_iam_policy.node_permissions.arn
-# }
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "webapp-ec2-instance-profile"
+  role = aws_iam_role.ec2_ssm.name
+}
+
+# IAM Role for Terraform
+# Get AWS account id
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "git_ssm_tf" {
+  name = "GitHubActions-Trust-Policy-TF"
+  assume_role_policy = jsonencode({ # Trust Policy
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Effect = "Allow"
+        #Resource = "*"
+
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.GITHUB_USERNAME}/${var.TF_REPO}:*"
+          }
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Custom Permission Policy
+resource "aws_iam_policy" "git_permission_policies_tf" {
+  name = "GitHubActions-Custom-Permission-Policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:*",
+          "elasticloadbalancing:*",
+          "acm:*",
+          "route53:*",
+          "s3:*",
+          "iam:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+  #   policy = <<EOT
+  # {
+  #   "Version": "2012-10-17",
+  #   "Statement": [
+  #     {
+  #       "Effect": "Allow",
+  #       "Action": [
+  #         "iam:*"
+  #       ]
+  #       "Resource": "*"
+  #     }
+  #   ]
+  # }
+  #     EOT
+
+}
+
+resource "aws_iam_role_policy_attachment" "git_attach_policies" {
+  role       = "Access_TF_resources_arch1"
+  policy_arn = aws_iam_policy.git_permission_policies_tf.arn
+}
+
+# IAM Role for WebApp Deployment
+resource "aws_iam_role" "git_ssm_app" {
+  name = "GitHubActions-Trust-Policy-WA"
+  assume_role_policy = jsonencode({ # Trust Policy
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Effect = "Allow"
+        #Resource = "*"
+
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.GITHUB_USERNAME}/${var.APP_REPO}:*"
+          }
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
 
 
-# /* Minimum Parameters: 
-#     - name:                 custom name for policy list
-#     - policy:               (list of permissions to allow for whoever wears the hat)
-# */
-# // Source: https://docs.aws.amazon.com/aws-managed-policy/latest/reference/AmazonEKSWorkerNodePolicy.html
-# resource "aws_iam_policy" "node_permissions" { //From AmazonEKSWorkerNodePolicy
-#   name = "Node-Permissions-List"
-#   policy = jsonencode({
+resource "aws_iam_policy" "github_webapp_deploy_policy" {
+  name = "github-actions-webapp-deploy-policy"
 
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Sid" : "WorkerNodePermissions",
-#         "Effect" : "Allow",
-#         "Action" : [
-#           "ec2:DescribeInstances",
-#           "ec2:DescribeInstanceTypes",
-#           "ec2:DescribeRouteTables",
-#           "ec2:DescribeSecurityGroups",
-#           "ec2:DescribeSubnets",
-#           "ec2:DescribeVolumes",
-#           "ec2:DescribeVolumesModifications",
-#           "ec2:DescribeVpcs",
-#           "eks:DescribeCluster",
-#           "eks-auth:AssumeRoleForPodIdentity"
-#         ],
-#         "Resource" : "*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:SendCommand",
+          "ssm:GetCommandInvocation",
+          "ssm:ListCommandInvocations",
+          "ec2:DescribeInstances"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_webapp_deploy_attach" {
+  role       = aws_iam_role.git_ssm_app.name
+  policy_arn = aws_iam_policy.github_webapp_deploy_policy.arn
+}
--- main.tf
+++ main.tf
@@ -1,16 +1,7 @@
 provider "aws" {
-  region = var.my_region
+  region = "us-east-1"
 }
 
-// Source: https://developer.hashicorp.com/terraform/tutorials/kubernetes/helm-provider?in=terraform%2Fkubernetes
-provider "helm" {
-  kubernetes = {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-      command     = "aws"
-    }
-  }
-}
+
+
+
