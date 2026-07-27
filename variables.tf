e37c1af5d9e53bf939d74c0aafdef7d679264545
Robert Heng <robertheng3@gmail.com>
2026-07-27 13:31:33 -0400

    added helm configurations

--- variables.tf
+++ variables.tf
@@ -1,11 +1,34 @@
-variable "GITHUB_USERNAME" {
-  type = string
+variable "my_region" {
+  description = "Region where the resource(s) will be managed"
+  type        = string
+  default     = null
 }
 
-variable "APP_REPO" {
-  type = string
+variable "region_a" {
+  type    = string
+  default = null
 }
 
-variable "TF_REPO" {
-  type = string
+variable "region_b" {
+  type    = string
+  default = null
 }
+
+variable "db_password" {
+  type    = string
+  default = null
+}
+
+# locals {
+#   private_subnet_a_id = [
+#     aws_subnet.private_1a.id,
+#     aws_subnet.private_2a.id
+#   ]
+# }
+
+# locals {
+#   private_subnet_b_id = [
+#     aws_subnet.private_1b.id,
+#     aws_subnet.private_2b.id
+#   ]
+# }
